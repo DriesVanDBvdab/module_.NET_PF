be387df0d6c6e79e27a43ff8bb56814394c5e8af
Dries VanDB <dries.vandenbussch@vdabcampus.be>
2026-04-10 15:03:22 +0200

    Eerste oefeningen met C# testen van visual studio functies

---
+++ PF/PFCursus/CodeFile2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PFCursus
+{
+    internal class CodeFile2
+    {
+    }
+}
---
+++ PF/PFCursus/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello,"+
+    " World!");
+Console.WriteLine("Asterix");
+Console.WriteLine("Obelix");
+Console.ReadKey(); //1
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.ForegroundColor = ConsoleColor.Red; //2
+Console.Clear(); //3
+Console.SetCursorPosition(10, 2);
+Console.WriteLine("Suske"); //4
+Console.SetCursorPosition(25, 6);
+Console.WriteLine("Wiske");
