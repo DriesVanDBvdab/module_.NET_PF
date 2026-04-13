59e50567527e645f6181441ab8c94c2ac1207645
Dries VanDB <dries.vandenbussch@vdabcampus.be>
2026-04-13 13:05:47 +0200

    Theory en oefeningen van deel 11 en 12 gedaan

--- PF/PFCursus/Program.cs
+++ PF/PFCursus/Program.cs
@@ -40,10 +40,110 @@
 //Console.WriteLine(cm);
 //Console.WriteLine(inch);
 
-const int BtwNummer = 213252520; // (1)
-int deeltal = BtwNummer / 100; // (2)
-int rest = deeltal % 97; // (3)
-int controle = BtwNummer % 100; // (4)
-Console.Write("97 - de rest van de deling van de eerste 7 cijfers door 97: "); 
-Console.WriteLine(97 - rest); // (5)
-Console.Write("Twee laatste cijfers: "); Console.WriteLine(controle); // (6)
+//const int BtwNummer = 213252520; // (1)
+//int deeltal = BtwNummer / 100; // (2)
+//int rest = deeltal % 97; // (3)
+//int controle = BtwNummer % 100; // (4)
+//Console.Write("97 - de rest van de deling van de eerste 7 cijfers door 97: "); 
+//Console.WriteLine(97 - rest); // (5)
+//Console.Write("Twee laatste cijfers: "); Console.WriteLine(controle); // (6)
+
+
+//11 datum en tijd
+//DateTime beginTijdRekening = new DateTime();
+//DateTime beginEuro = new DateTime(2002, 1, 1);
+//DateTime mijnGeboortedatum = new DateTime(1966, 8, 1, 10, 15, 0);
+//Console.WriteLine(beginTijdRekening);
+//Console.WriteLine(beginEuro);
+//Console.WriteLine(mijnGeboortedatum);
+
+//DateTime datum1 = new DateTime(2022, 1, 1);
+//DateTime datum2 = new DateTime(2022, 1, 1);
+//DateTime datum3 = new DateTime(2022, 1, 2);
+//Console.WriteLine(datum1 == datum2); //true
+//Console.WriteLine(datum2 >= datum3);
+
+//DateTime factuurDatum = new DateTime(2023, 1, 1); 
+//Console.WriteLine(factuurDatum.AddDays(40)); 
+//DateTime uitersteBetaalDatum = factuurDatum.AddDays(40);
+//Console.WriteLine(uitersteBetaalDatum);
+
+//DateTime begindatum = new DateTime(2023, 1, 1, 8, 0, 0);
+//DateTime einddatum = new DateTime(2023, 2, 1, 12, 30, 50);
+//Console.WriteLine(begindatum);
+//Console.WriteLine(einddatum);
+//Console.WriteLine();
+//TimeSpan interval = einddatum - begindatum;
+//Console.WriteLine(interval.ToString());
+
+//Console.WriteLine(interval.Days); //onderdeel aantal dagen
+//Console.WriteLine(interval.TotalDays); //totaal aantal dagen als een decimaal getal
+//Console.WriteLine(interval.Hours); //onderdeel aantal uren (range -23 tot 23)
+//Console.WriteLine(interval.TotalHours); //totaal aantal uren
+//Console.WriteLine(interval.Minutes); //onderdeel aantal minuten (range -59 tot 59)
+//Console.WriteLine(interval.TotalMinutes);//totaal aantal minuten
+//Console.WriteLine(interval.Seconds); //onderdeel seconden (range -59 tot 59)
+//Console.WriteLine(interval.TotalSeconds);//totaal aantal seconden
+//Console.WriteLine(interval.Milliseconds);//onderdeel aantal milliseconden
+////(range -999 tot 999)
+//Console.WriteLine(interval.TotalMilliseconds); //totaal aantal milliseconden
+//Console.WriteLine(interval.Ticks); //1 milliseconde = 10000 ticks
+
+//DateTime vandaag = DateTime.Now; 
+//DateOnly datumVandaag = DateOnly.FromDateTime(vandaag); 
+//TimeOnly nu = TimeOnly.FromDateTime(vandaag);
+//Console.WriteLine(vandaag);
+//Console.WriteLine(datumVandaag);
+//Console.WriteLine(nu);
+
+//12 Het type string
+//using System.Text;
+
+//string krijger1 = "Asterix";
+//string krijger2 = "Obelix";
+//string samen = krijger1 + " en " + krijger2;
+//Console.WriteLine(samen);
+
+//StringBuilder bericht = new StringBuilder("hallo"); // (2)
+//Console.WriteLine(bericht);
+//bericht.Append(" iedereen"); // (4)
+//Console.WriteLine(bericht);
+
+//string krijger1 = "Asterix";
+//string krijger2 = "Obelix";
+//string groteKrijger = "Asterix";
+//string kleineKrijger = "asterix";
+//Console.WriteLine(krijger1 == krijger2); // (1)
+//Console.WriteLine(krijger1 == groteKrijger); // (2)
+//Console.WriteLine(krijger1 == kleineKrijger); // (3)
+//Console.WriteLine(krijger1.CompareTo(krijger2));
+
+//string kinderenWoord = "7";
+//byte kinderen = Convert.ToByte(kinderenWoord);
+
+//const float CmInch = 2.54f;
+//Console.Write("Tik een lengte in cm: ");
+//float cm = float.Parse(Console.ReadLine());
+//Console.Write("lengte in inch: "); Console.WriteLine(cm/CmInch); 
+//Console.Write("Geboortedatum: "); 
+//DateTime geboortedatum = DateTime.Parse(Console.ReadLine()); 
+//Console.WriteLine(geboortedatum);
+
+//string tekst = null; Console.WriteLine(string.IsNullOrEmpty(tekst)); //geeft True want tekst is null
+//tekst = string.Empty; Console.WriteLine(string.IsNullOrEmpty(tekst)); //geeft True want tekst is leeg
+//tekst = "\t \n"; Console.WriteLine(string.IsNullOrEmpty(tekst)); //geeft False
+//tekst = "hallo"; Console.WriteLine(string.IsNullOrEmpty(tekst)); //geeft False
+
+//string tekst = null; Console.WriteLine(string.IsNullOrWhiteSpace(tekst)); //geeft True
+//tekst = string.Empty; Console.WriteLine(string.IsNullOrWhiteSpace(tekst)); //geeft True
+//tekst = "\t \n"; Console.WriteLine(string.IsNullOrWhiteSpace(tekst)); //geeft True
+//tekst = "hallo"; Console.WriteLine(string.IsNullOrWhiteSpace(tekst)); //geeft False
+
+
+//var getal = 15.365f;
+//Console.WriteLine("{0,-10}", getal);
+//Console.WriteLine("{0, 10}", getal);
+//Console.WriteLine("{0, 7:N1}", getal);
+//DateTime tijd = DateTime.UtcNow;
+//Console.WriteLine("{0,30}", tijd);
+
--- PF/PFOefeningen/Program.cs
+++ PF/PFOefeningen/Program.cs
@@ -12,21 +12,41 @@
 //Console.WriteLine($"{seconden} seconden is gelijk aan {uren} uur, {minuten} minuten en {resterendeSeconden2} seconden.");
 
 //Oefening 10.8.3 Snoepautomaat
-const int kitKatPrijs = 132;
+//const int kitKatPrijs = 132;
 
-const int betaalBedrag = 200;
-int aantalEuroKitKat = (betaalBedrag - kitKatPrijs) / 100;
-int eurocentenKitKat = (betaalBedrag - kitKatPrijs) % 100;
-int aantal50CentStukkenKitKat = eurocentenKitKat / 50;
-int aantal20CentStukkenKitKat = (eurocentenKitKat % 50) / 20;
-int aantal10CentStukkenKitKat = ((eurocentenKitKat % 50) % 20) / 10;
-int aantal5CentStukkenKitKat = (((eurocentenKitKat % 50) % 20) % 10) / 5;
-int aantal2CentStukkenKitKat = ((((eurocentenKitKat % 50) % 20) % 10) % 5) / 2;
-int aantal1CentStukkenKitKat = ((((eurocentenKitKat % 50) % 20) % 10) % 5) % 2;
-Console.WriteLine($"terug krijgen voor kitkat: {aantalEuroKitKat} euro's, "+
-    $"{aantal50CentStukkenKitKat}x50cent, "+
-    $"{aantal20CentStukkenKitKat}x20cent, "+
-    $"{aantal10CentStukkenKitKat}x10cent, "+
-    $"{aantal5CentStukkenKitKat}x5cents, "+
-    $"{aantal2CentStukkenKitKat}x2cents, "+
-    $"{aantal1CentStukkenKitKat}x1cents");
+//const int betaalBedrag = 200;
+//int aantalEuroKitKat = (betaalBedrag - kitKatPrijs) / 100;
+//int eurocentenKitKat = (betaalBedrag - kitKatPrijs) % 100;
+//int aantal50CentStukkenKitKat = eurocentenKitKat / 50;
+//int aantal20CentStukkenKitKat = (eurocentenKitKat % 50) / 20;
+//int aantal10CentStukkenKitKat = ((eurocentenKitKat % 50) % 20) / 10;
+//int aantal5CentStukkenKitKat = (((eurocentenKitKat % 50) % 20) % 10) / 5;
+//int aantal2CentStukkenKitKat = ((((eurocentenKitKat % 50) % 20) % 10) % 5) / 2;
+//int aantal1CentStukkenKitKat = ((((eurocentenKitKat % 50) % 20) % 10) % 5) % 2;
+//Console.WriteLine($"terug krijgen voor kitkat: {aantalEuroKitKat} euro's, "+
+//    $"{aantal50CentStukkenKitKat}x50cent, "+
+//    $"{aantal20CentStukkenKitKat}x20cent, "+
+//    $"{aantal10CentStukkenKitKat}x10cent, "+
+//    $"{aantal5CentStukkenKitKat}x5cents, "+
+//    $"{aantal2CentStukkenKitKat}x2cents, "+
+//    $"{aantal1CentStukkenKitKat}x1cents");
+
+// oefening 12.12 Strings
+using System.Text;
+
+Console.WriteLine("Geef de volgende gegevens in voor u wachtwoord:");
+Console.Write("AcherNaam: ");
+string familieNaam = Console.ReadLine() ?? string.Empty;
+Console.Write("Zonenummer: ");
+string zoneNummer = Console.ReadLine() ?? string.Empty;
+Console.Write("Postcode: ");
+string postcode = Console.ReadLine() ?? string.Empty;
+
+StringBuilder wachtwoord = new StringBuilder(familieNaam.Substring(1, 1).ToLower());
+wachtwoord.Append(familieNaam.Substring(0, 1).ToUpper());
+wachtwoord.Append(zoneNummer.Remove(0,1));
+int postcodeKwadraat = int.Parse(postcode.Substring(3,1)) * int.Parse(postcode.Substring(3, 1));
+wachtwoord.Append(postcodeKwadraat.ToString());
+wachtwoord.Insert(3, "*");
+
+Console.WriteLine($"Uw wachtwoord is: ({wachtwoord})");
